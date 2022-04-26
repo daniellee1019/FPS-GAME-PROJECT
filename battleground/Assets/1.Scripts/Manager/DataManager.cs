2a1dacc1420d09301c4b8f4d45cf83985f6673ae
이정민 <79199117+daniellee1019@users.noreply.github.com>
2022-04-26 15:22:16 +0900

    DataManager,EffectManager,Soundclip 스크립트 작성
    이펙트의 데이터를 get,  load를 통해 EffectManager로 관리를 해주고, SoundClip을 이용해서 루프, 페이드 관련 속성, 오디오 클립 속성들을 설정해준다.

---
+++ battleground/Assets/1.Scripts/Manager/DataManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 이펙트의 Data들을 읽고 불러오는, (get, load) 데이타 홀더 같은 개념  
+/// </summary>
+public class DataManager : MonoBehaviour
+{
+    private static EffectData effectData = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(effectData == null)
+        {
+            effectData = ScriptableObject.CreateInstance<EffectData>();
+            effectData.LoadData();
+        }
+    }
+
+    public static EffectData EffectData()
+    {
+        if (effectData == null)
+        {
+            effectData = ScriptableObject.CreateInstance<EffectData>();
+            effectData.LoadData();
+        }
+        return effectData;
+    }
+
+}
