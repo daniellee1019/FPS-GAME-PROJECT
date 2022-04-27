87188aa12bd8f117df9fa7dbcaae95ee075dc0cd
이정민 <79199117+daniellee1019@users.noreply.github.com>
2022-04-27 19:35:19 +0900

    SoundManager 스크립트 작성
    Sound managing에 필요한 메서드 맴버 변수 추가 및 DataManager에 SoundData 로드 기능 추가

--- battleground/Assets/1.Scripts/Manager/DataManager.cs
+++ battleground/Assets/1.Scripts/Manager/DataManager.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// 이펙트의 Data들을 읽고 불러오는, (get, load) 데이타 홀더 같은 개념  
+/// 이펙트, 사운드 등 여러 Data들을 읽고 불러오는, (get, load) 데이터 홀더 같은 개념  
 /// </summary>
 public class DataManager : MonoBehaviour
 {
+    private static SoundData soundData = null;
     private static EffectData effectData = null;
     // Start is called before the first frame update
     void Start()
@@ -13,6 +14,11 @@ public class DataManager : MonoBehaviour
         if(effectData == null)
         {
             effectData = ScriptableObject.CreateInstance<EffectData>();
+            effectData.LoadData();
+        }
+        if(soundData == null)
+        {
+            soundData = ScriptableObject.CreateInstance<SoundData>();
             effectData.LoadData();
         }
     }
@@ -25,6 +31,15 @@ public class DataManager : MonoBehaviour
             effectData.LoadData();
         }
         return effectData;
+    }
+    public static SoundData SoundData()
+    {
+        if(soundData == null)
+        {
+            soundData = ScriptableObject.CreateInstance<SoundData>();
+            soundData.LoadData();
+        }
+        return soundData;
     }
 
 }
---
+++ battleground/Assets/1.Scripts/Manager/SoundManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : SingletonMonobehaviour<SoundManager>
+{
+    public const string MasterGroupName = "Master";
+    public const string EffectGroupName = "Effect";
+    public const string BGMGroupName = "BGM";
+    public const string UIGroupName = "UI";
+    public const string MixerName = "AudioMixer";
+    public const string ContainerName = "SoundContainer";
+    public const string FadeA = "FadeA";
+    public const string FadeB = "FadeB";
+    public const string UI = "UI";
+    public const string EffectVolumeParam = "Volume_Effect";
+    public const string BGMVolumeParam = "Volume_BGM";
+    public const string UIVolumeParam = "Volume_UI";
+
+    public enum MusicPlayingType
+    {
+        None = 0,
+        SourceA = 1, // 페이드 관련
+        SourceB = 2,
+        AtoB = 3, // 배경음 관련
+        BtoA = 4,
+    }
+
+    public AudioMixer mixer = null;
+    public Transform audioRoot = null;
+    public AudioSource fadeA_audio = null;
+    public AudioSource fadeB_audio = null;
+    public AudioSource[] effect_audios = null; // 많은 이펙트 사운드가 동시에 재생되면 오디오가 찢어지기에 이펙트 사운드 채널 개수를 제한을 두어 사전에 방지한다. 
+    public AudioSource UI_audio = null;
+
+    public float[] effect_PlayStartTime = null; //더이상 이펙트 사운드 채널이 추가될 공간이 없다면 가장 오래된 사운드를 끄고 new sound 재생 
+    private MusicPlayingType currentPlayingType = MusicPlayingType.None;
+    private bool isTricking = false;
+    private SoundClip currentSound = null;
+    private SoundClip lastClip = null;
+    private float minVolume = -80.0f;
+    private float maxVolume = 0.0f;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
