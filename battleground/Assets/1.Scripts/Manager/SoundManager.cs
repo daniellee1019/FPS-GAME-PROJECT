26a1e62ff52a825c363cd0836f40b8472060acd5
이정민 <79199117+daniellee1019@users.noreply.github.com>
2022-04-30 15:48:22 +0900

    오디오 볼륨 조절 기능 추가(1)
    이팩트 오디오 볼륨 속성 및 조절 비율 속성 추가

--- battleground/Assets/1.Scripts/Manager/SoundManager.cs
+++ battleground/Assets/1.Scripts/Manager/SoundManager.cs
@@ -36,6 +36,7 @@ public class SoundManager : SingletonMonobehaviour<SoundManager>
     public AudioSource UI_audio = null;
 
     public float[] effect_PlayStartTime = null; //더이상 이펙트 사운드 채널이 추가될 공간이 없다면 가장 오래된 사운드를 끄고 new sound 재생 
+    private int EffectChannelCount = 5;
     private MusicPlayingType currentPlayingType = MusicPlayingType.None;
     private bool isTricking = false;
     private SoundClip currentSound = null;
@@ -46,7 +47,81 @@ public class SoundManager : SingletonMonobehaviour<SoundManager>
 
     void Start()
     {
-        
+        if(this.mixer == null)
+        {
+            this.mixer = Resources.Load(MixerName) as AudioMixer;
+        }
+        if( this.audioRoot == null)
+        {
+            audioRoot = new GameObject(ContainerName).transform;
+            audioRoot.SetParent(transform);
+            audioRoot.localPosition = Vector3.zero;
+        }
+        if(fadeA_audio != null)
+        {
+            GameObject fadeA = new GameObject(FadeA, typeof(AudioSource));
+            fadeA.transform.SetParent(audioRoot);
+            this.fadeA_audio = fadeA.GetComponent<AudioSource>(); // 사운드 재생.
+            this.fadeA_audio.playOnAwake = false;
+        }
+        if(fadeB_audio == null)
+        {
+            GameObject fadeB = new GameObject(FadeB, typeof(AudioSource));
+            fadeB.transform.SetParent(audioRoot);
+            fadeB_audio = fadeB.GetComponent<AudioSource>();
+            fadeB_audio.playOnAwake = false;
+        }
+        if(UI_audio == null)
+        {
+            GameObject ui = new GameObject(UI, typeof(AudioSource));
+            ui.transform.SetParent(audioRoot);
+            UI_audio = ui.GetComponent<AudioSource>();
+            UI_audio.playOnAwake = false;
+        }
+        if(this.effect_audios == null && this.effect_audios.Length == 0)
+        {
+            this.effect_PlayStartTime = new float[EffectChannelCount];
+            this.effect_audios = new AudioSource[EffectChannelCount];
+            for(int i = 0; i < EffectChannelCount; i++)
+            {
+                effect_PlayStartTime[i] = 0.0f;
+                GameObject effect = new GameObject("Effect" + i.ToString(), typeof(AudioSource));
+                effect.transform.SetParent(audioRoot);
+                this.effect_audios[i] = effect.GetComponent<AudioSource>();
+                this.effect_audios[i].playOnAwake = false;
+            }
+        }
+        if (mixer != null)
+        {
+            this.fadeA_audio.outputAudioMixerGroup = mixer.FindMatchingGroups(BGMVolumeParam)[0];
+            this.fadeB_audio.outputAudioMixerGroup = mixer.FindMatchingGroups(BGMVolumeParam)[0];// 오디오 볼륨 조절.
+            this.UI_audio.outputAudioMixerGroup = mixer.FindMatchingGroups(UIVolumeParam)[0];
+            for (int i = 0; i < this.effect_audios.Length; i++) // 이펙트
+            {
+                this.effect_audios[i].outputAudioMixerGroup = mixer.FindMatchingGroups(EffectGroupName)[0];
+            }
+        }
+
+    }
+
+    public void SetBGMVolume(float currentRatio)//현재비율
+    {
+        currentRatio = Mathf.Clamp01(currentRatio); // 0과 1 사이에 값을 정해줌.
+        float voluem = Mathf.Lerp(minVolume, maxVolume, currentRatio);
+        this.mixer.SetFloat(BGMVolumeParam, voluem);
+        PlayerPrefs.SetFloat(BGMVolumeParam, voluem);
+    }
+
+    public float GetBGMVolume()
+    {
+        if (PlayerPrefs.HasKey(BGMVolumeParam))
+        {
+            return Mathf.Lerp(minVolume, maxVolume, PlayerPrefs.GetFloat(BGMVolumeParam));
+        }
+        else
+        {
+            return maxVolume;
+        }
     }
 
     void Update()
